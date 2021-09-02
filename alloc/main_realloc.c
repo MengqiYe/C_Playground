7b577c7de26cf6ad91bb0c7bd8222941d16e5cf0
elsey <elsey@wonderland>
2021-09-02 16:23:28 +0800

    Test realloc.

--- alloc/main_realloc.c
+++ alloc/main_realloc.c
@@ -12,21 +12,22 @@ typedef struct rect{
 }rect;
 
 int main(){
-    int max_array_size = 3;
+    int max_array_size = 32;
 
     rect *detections = calloc(3, sizeof(rect));
     int actual_detection_count = 0;
 
     for (int i = 0; i < max_array_size; ++i) {
-//        detections = realloc(detections, sizeof(rect) * ++actual_detection_count);
+        detections = realloc(detections, sizeof(rect) * ++actual_detection_count);
 //        rect r = detections[i];
 //        r.x = 99;
         rect *r = &detections[i];
         r->x = i;
-        printf("pointer address : %X\n", detections);
+        printf("pointer address : %X\t", detections);
 
         for (int j = 0; j < i + 1; ++j) {
-            printf("rect %d, x : %d\n", j, detections[j].x);
+            printf("rect %d, x : %d\t", j, detections[j].x);
         }
+        printf("\n");
     }
 }
